d2e0a2bfac66380c8042e567d171af2e8c16bfdf
mattez1 <133816825+mattez1@users.noreply.github.com>
2024-10-13 16:24:57 +0100

    attack logic + game manager base

--- Assets/KeenanScripts/Card.cs
+++ Assets/KeenanScripts/Card.cs
@@ -9,4 +9,22 @@ public class Card : ScriptableObject
     public int health;
     public int manaRequired;
     public string manaTypeRequired;
+
+   // matt modifications 
+    public void Attack(Card targetCard)
+    {
+        
+        targetCard.health -= this.cardDamage;
+
+       
+        if (targetCard.health <= 0)
+        {
+            Debug.Log($"{targetCard.name} has been defeated by {this.name}!");
+        }
+        else
+        {
+            Debug.Log($"{targetCard.name} now has {targetCard.health} health remaining.");
+        }
+    }
+    //end
 }
