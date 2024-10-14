9f261e23ba79dccf394ef94cfdf8b79060dea9a6
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2024-10-14 10:26:02 +0100

    Moved the Attack() method
    Moved the Attack() method from the Card Scriptable Object to the CardBehaviour MonoBehaviour.

--- Assets/KeenanScripts/Card.cs
+++ Assets/KeenanScripts/Card.cs
@@ -10,21 +10,5 @@ public class Card : ScriptableObject
     public int manaRequired;
     public string manaTypeRequired;
 
-   // matt modifications 
-    public void Attack(Card targetCard)
-    {
-        
-        targetCard.health -= this.cardDamage;
-
-       
-        if (targetCard.health <= 0)
-        {
-            Debug.Log($"{targetCard.name} has been defeated by {this.name}!");
-        }
-        else
-        {
-            Debug.Log($"{targetCard.name} now has {targetCard.health} health remaining.");
-        }
-    }
-    //end
+   
 }
--- Assets/KeenanScripts/CardBehaviour.cs
+++ Assets/KeenanScripts/CardBehaviour.cs
@@ -3,4 +3,22 @@ using UnityEngine;
 public class CardBehaviour : MonoBehaviour
 {
     [SerializeField] Card cardStats;
+
+    // matt modifications 
+    public void Attack(Card targetCard)
+    {
+
+        targetCard.health -= cardStats.cardDamage;
+
+
+        if (targetCard.health <= 0)
+        {
+            Debug.Log($"{targetCard.name} has been defeated by {cardStats.name}!");
+        }
+        else
+        {
+            Debug.Log($"{targetCard.name} now has {targetCard.health} health remaining.");
+        }
+    }
+    //end
 }
