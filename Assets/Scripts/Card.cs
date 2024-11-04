f433804d5e5632ed7e2203b1d01bc1f107ffde89
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-04 14:51:32 +0000

    Add CardAttack class
    Start working on the CardAttack class to the cards to allow them to attack.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -215,7 +215,27 @@ public class CardInfo : MonoBehaviour
 //Keenan Addition
 public class CardAttack : MonoBehaviour
 {
+    public void Attack(CardInfo targetCard)
+    {
+        targetCard.defenseValue -= GetComponent<CardInfo>().attackValue;
 
+
+        if (targetCard.defenseValue <= 0)
+        {
+            Debug.Log($"{targetCard.gameObject.name} has been defeated by {gameObject.name}!");
+        }
+        else
+        {
+            Debug.Log($"{targetCard.gameObject.name} now has {targetCard.defenseValue} health remaining.");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //Select the card.
+        Debug.Log("The card \"" + gameObject.name + "\" has been clicked");
+        //FindAnyObjectByType<GameManager>().SelectCard(gameObject);
+    }
 }
 //End
 
