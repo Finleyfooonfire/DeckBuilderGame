fd87f82757fb22366499ea846c07bf9f80fb4a23
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-05 15:58:54 +0000

    Created the Serialize function
    Made the Serialize function for the NetworkSerializer class. It takes in a transform (the PlayingField), gets all the data needed to reconstruct it on the other computer, and converts it into a string.

--- Assets/Scripts/Networking/NetworkSerializer.cs
+++ Assets/Scripts/Networking/NetworkSerializer.cs
@@ -2,19 +2,78 @@ using UnityEngine;
 static class NetworkSerializer
 {
     //Convert changes ingame into a string that can be sent on the network.
-    static string Serialize(Transform playingField)
+    public static string Serialize(Transform playingField)
     {
-        Transform cardPlayArea = playingField.Find("CardPlayArea");
-        
-        Transform playerGrave = playingField.Find("PlayerGrave");
-        Transform playerHand = playingField.Find("PlayerHand");
-        Transform playerDeck = playingField.Find("PlayerDeck");
-
-        Transform opponentGrave = playingField.Find("OpponentGrave");
-        Transform opponentHand = playingField.Find("OpponentHand");
-        Transform opponentDeck = playingField.Find("OpponentDeck");
-
         string output = "";
+        //Get the number of cards in each area and then add the card data.
+        Transform cardPlayArea = playingField.Find("CardPlayArea");
+        output += cardPlayArea.childCount.ToString();
+        output += "\n";//Separate pieces of data with a '\n'
+        foreach (Transform x in cardPlayArea)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+
+        //Serialize all player card stats.
+        //Grave cards
+        Transform playerGrave = playingField.Find("Player").Find("PlayerGrave");
+        output += playerGrave.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in playerGrave)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+        //Hand cards
+        Transform playerHand = playingField.Find("Player").Find("PlayerHand");
+        output += playerHand.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in playerHand)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+        //Deck cards
+        Transform playerDeck = playingField.Find("Player").Find("PlayerDeck");
+        output += playerDeck.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in playerDeck)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+
+
+        //Serialize all opponent card stats
+        //Grave cards
+        Transform opponentGrave = playingField.Find("Opponent").Find("OpponentGrave");
+        output += opponentGrave.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in opponentGrave)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+        //Hand cards
+        Transform opponentHand = playingField.Find("Opponent").Find("OpponentHand");
+        output += opponentHand.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in opponentHand)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+        //Deck cards
+        Transform opponentDeck = playingField.Find("Opponent").Find("OpponentDeck");
+        output += opponentDeck.childCount.ToString();
+        output += "\n";
+        foreach (Transform x in opponentDeck)
+        {
+            output += x.GetComponent<Card>().stats.name;
+            output += "\n";
+        }
+
 
         //Conver the data into the string here.
 
