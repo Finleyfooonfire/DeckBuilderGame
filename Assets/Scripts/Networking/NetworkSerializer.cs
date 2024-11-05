8c099b5c10a138da76867f452ce3cc9643ecbbbc
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-05 17:54:04 +0000

    Add some more things to the Deserialize function.
    The function is to convert the inputted string into a replica of what the other device serialized and sent over the network.
    The Deserialize function is not yet functional.

--- Assets/Scripts/Networking/NetworkSerializer.cs
+++ Assets/Scripts/Networking/NetworkSerializer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 static class NetworkSerializer
 {
@@ -87,23 +88,109 @@ static class NetworkSerializer
     }
 
     //Convert a string that can be sent on the network into changes ingame.
+    private enum Stage
+    {
+        None = 0,
+        CardPlayAreaCardCount = 1,
+        CardPlayAreaCardsName = 2,
+        CardPlayAreaCardsHealth = 3,
+        CardPlayAreaCardsOwner = 4,
+        PlayerGraveyardCardCount = 5,
+        PlayerGraveyardCards = 6,
+        PlayerHandCardCount = 7,
+        PlayerHandCards = 8,
+        PlayerDeckCardCount = 9,
+        PlayerDeckCards = 10,
+        OpponentGraveyardCardCount = 11,
+        OpponentGraveyardCards = 12,
+        OpponentHandCardCount = 13,
+        OpponentHandCards = 14,
+        OpponentDeckCardCount = 15,
+        OpponentDeckCards = 16,
+        Finished = 17,
+    }
     static Transform Deserialize(Transform playingField, string input)
     {
+        //Initialize deserialize function variables
+        Stage stage = Stage.None;
+        int itemCount = 0;
+        string subString = "";
+
+        //Deserialized data
+        int numberOfCards = 0;
+
         //Convert the string into the game thingy
         //iterate through the input a character at a time.
         foreach (char c in input)
         {
-            //See if any of the characters are special
-            switch (c) {
-                case '\n':
+            if (c != '\n')//Build up the substring until it reaches the end.
+            {
+                subString += c;
+            }
+            else//Once the end is reached, do something. Then go to the next stage
+            {
+                //Different things happen depending on the stage that is in progress.
+                switch (stage)
+                {
+                    case Stage.CardPlayAreaCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.CardPlayAreaCardsName:
 
-                    break;
-                default:
-                    break;
+                        break;
+                    case Stage.CardPlayAreaCardsHealth:
+
+                        break;
+                    case Stage.CardPlayAreaCardsOwner:
+                        numberOfCards--;//Reduce the number of cards left to look at.
+                        break;
+                    case Stage.PlayerGraveyardCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.PlayerGraveyardCards:
+
+                        break;
+                    case Stage.PlayerHandCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.PlayerHandCards:
+
+                        break;
+                    case Stage.PlayerDeckCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.PlayerDeckCards:
+
+                        break;
+                    case Stage.OpponentGraveyardCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.OpponentGraveyardCards:
+
+                        break;
+                    case Stage.OpponentHandCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.OpponentHandCards:
+
+                        break;
+                    case Stage.OpponentDeckCardCount:
+                        numberOfCards = int.Parse(subString);
+                        break;
+                    case Stage.OpponentDeckCards:
+
+                        break;
+                    case Stage.Finished:
+
+                        break;
+                }
+                //Clear the substring and go to the next stage.
+                subString = "";
+                stage += 1;
             }
         }
-
-
+        
+        
         return playingField;
     }
 }
