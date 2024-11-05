af5859e4d7de8e11f343e95c9e58192ed98bb130
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-05 12:17:35 +0000

    Made game networking scripts.

---
+++ Assets/Scripts/Networking/GameNetworkManager.cs
@@ -0,0 +1,81 @@
+using System.Net.Sockets;
+using System.Net;
+using UnityEngine;
+using TMPro;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    bool isHost;
+    [SerializeField] GameObject server;
+    [SerializeField] GameObject client;
+    [SerializeField] GameObject serverUI;
+    [SerializeField] GameObject clientUI;
+    [SerializeField] GameObject startButtons;
+    [SerializeField] TMP_InputField IPInput;
+    [SerializeField] GameObject IPButton;
+    
+
+    public void OnStartGame(bool setHost)
+    {
+        startButtons.SetActive(false);
+        isHost = setHost;
+        StartUpNetwork();
+    }
+
+    //Gets the local IP address
+    string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public string deviceIP;
+
+
+    void StartUpNetwork()
+    {
+        
+        if (isHost)
+        {
+            server.SetActive(true);
+            serverUI.SetActive(true);
+            clientUI.SetActive(true);
+            IPInput.gameObject.SetActive(false);
+            IPButton.SetActive(false);
+            //Get this device IP and used it to connect the TCPClient to the TCPServer
+            deviceIP = GetLocalIPAddress();
+            //client.GetComponent<TCPClient1>().OnAttemptConnectToServer(deviceIP);
+        }
+        else
+        {
+            server.SetActive(false);
+            serverUI.SetActive(false);
+            clientUI.SetActive(true);
+            //Get the IP from the input box and attempt to connect
+            deviceIP = IPInput.text;
+            Debug.Log(deviceIP);
+        }
+    }
+
+    public void OnConnectButton()
+    {
+        // Update deviceIP right before connection
+        deviceIP = IPInput.text;
+        Debug.Log("Attempting to connect to IP: " + deviceIP);
+
+        // Attempt to connect
+        client.GetComponent<TCPClient1>().OnAttemptConnectToServer(deviceIP);
+
+        // Hide input UI after initiating connection
+        IPInput.gameObject.SetActive(false);
+        IPButton.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Networking/GameTCPClient.cs
@@ -0,0 +1,82 @@
+
+using System;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using PimDeWitte.UnityMainThreadDispatcher;
+using JetBrains.Annotations;
+using System.Diagnostics.Contracts;
+
+public class GameTCPClient : MonoBehaviour
+{
+    private TcpClient client;
+    
+
+    public TMP_InputField inputField;  // Reference to the InputField UI component
+
+    
+
+    public void OnAttemptConnectToServer(string serverIP)
+
+    {
+
+        client = new TcpClient();
+        client.BeginConnect(serverIP, 7777, OnConnected, null);  // Non-blocking connect to the server
+        Debug.Log(serverIP);    
+    }
+
+    void OnConnected(IAsyncResult ar)
+    {
+        if (client.Connected)
+        {
+            Debug.Log("Connected to server!");
+            ReceiveMessages(); // Start receiving messages
+        }
+    }
+
+    void ReceiveMessages()
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] data = new byte[256];
+
+        stream.BeginRead(data, 0, data.Length, (IAsyncResult ar) =>
+        {
+            int bytesRead = stream.EndRead(ar);
+            string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+            Debug.Log("Received from server: " + message);
+
+
+
+            ReceiveMessages(); // Continue receiving messages
+        }, null);
+    }
+
+    public void SendMessageToServer()
+    {
+        if (client.Connected)
+        {
+            string messageToSend = inputField.text;
+            byte[] data = Encoding.ASCII.GetBytes(messageToSend);
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(data, 0, data.Length); // Send message to server
+
+            Debug.Log("Sent to server: " + messageToSend);
+            inputField.text = ""; // Clear the input field after sending
+        }
+        else
+        {
+            Debug.LogWarning("Client is not connected to the server!");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (client != null)
+        {
+            client.Close();  // Close the client connection when the application quits
+        }
+    }
+}
