b0b524631163225cf8f0cbd8b79aee21ae57242a
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-07 10:29:13 +0000

    Remove the NetworkSerializer test code

--- Assets/KeenanScripts/BoardState.cs
+++ Assets/KeenanScripts/BoardState.cs
@@ -9,18 +9,6 @@ public class BoardState : MonoBehaviour
     Transform board;
     Transform graveyard;
 
-    //Keenan addition: For debugging
-    float lTime;
-    void Update()
-    {
-        if (lTime + 5f >  Time.time) return;
-        string serializedData = NetworkSerializer.Serialize(gameObject.transform);
-        Debug.Log(serializedData);//Test to see if Serialize works
-        Debug.Log(transform.Equals(NetworkSerializer.Deserialize(transform, serializedData)));//Test to see if Deserialize works
-        lTime = Time.time;
-    }
-    //END
-
     void OnAddToBoard(int cardSelected)
     {
        hand.GetChild(cardSelected).parent = board;
