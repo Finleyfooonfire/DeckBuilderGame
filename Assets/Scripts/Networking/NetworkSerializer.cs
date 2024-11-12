a0007494b565304a05f1a29456e189e1e6bab75d
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-12 20:07:24 +0000

    Implement a simple Serialize method

--- Assets/Scripts/Networking/NetworkSerializer.cs
+++ Assets/Scripts/Networking/NetworkSerializer.cs
@@ -51,7 +51,74 @@ class NetworkSerializer
     ///TODO: Packaging Card movement data into packet to be sent: https://www.notion.so/finleyfooonfire/Decomposition-13c4b7e33ee880389e8be96f21928b4c
     public void Serialize(CardsChange cardsChange, ref DataStreamWriter writer)
     {
-        throw new NotImplementedException();
+        //Write the number of cards in the newCards list
+        writer.WriteByte((byte)cardsChange.newCards.Count);
+        //Write the cards in the newCards list
+        for (int i = 0; i < cardsChange.newCards.Count; i++)
+        {
+            writer.WriteByte((byte)cardsChange.newCards[i].manaCost);
+            writer.WriteByte((byte)cardsChange.newCards[i].attackValue);
+            writer.WriteByte((byte)cardsChange.newCards[i].defenseValue);
+            writer.WriteFixedString32((FixedString32Bytes)cardsChange.newCards[i].cardName);
+            writer.WriteByte((byte)cardsChange.newCards[i].faction);
+            writer.WriteByte((byte)cardsChange.newCards[i].cardType);
+        }
+
+        //Write the number of cards in the drawnCards list
+        writer.WriteByte((byte)cardsChange.drawnCards.Count);
+        //Write the cards in the drawnCards list
+        for (int i = 0; i < cardsChange.drawnCards.Count; i++)
+        {
+            writer.WriteByte((byte)cardsChange.drawnCards[i].manaCost);
+            writer.WriteByte((byte)cardsChange.drawnCards[i].attackValue);
+            writer.WriteByte((byte)cardsChange.drawnCards[i].defenseValue);
+            writer.WriteFixedString32((FixedString32Bytes)cardsChange.drawnCards[i].cardName);
+            writer.WriteByte((byte)cardsChange.drawnCards[i].faction);
+            writer.WriteByte((byte)cardsChange.drawnCards[i].cardType);
+        }
+
+
+        //Write the number of cards in the playedCards list
+        writer.WriteByte((byte)cardsChange.playedCards.Count);
+        //Write the cards in the playedCards list
+        for (int i = 0; i < cardsChange.playedCards.Count; i++)
+        {
+            writer.WriteByte((byte)cardsChange.playedCards[i].manaCost);
+            writer.WriteByte((byte)cardsChange.playedCards[i].attackValue);
+            writer.WriteByte((byte)cardsChange.playedCards[i].defenseValue);
+            writer.WriteFixedString32((FixedString32Bytes)cardsChange.playedCards[i].cardName);
+            writer.WriteByte((byte)cardsChange.playedCards[i].faction);
+            writer.WriteByte((byte)cardsChange.playedCards[i].cardType);
+        }
+
+
+        //Write the number of cards in the killedCards list
+        writer.WriteByte((byte)cardsChange.killedCards.Count);
+        //Write the cards in the killedCards list
+        for (int i = 0; i < cardsChange.killedCards.Count; i++)
+        {
+            writer.WriteByte((byte)(cardsChange.killedCards[i].isPlayerCard ? 1 : 0));
+            writer.WriteByte((byte)cardsChange.killedCards[i].manaCost);
+            writer.WriteByte((byte)cardsChange.killedCards[i].attackValue);
+            writer.WriteByte((byte)cardsChange.killedCards[i].defenseValue);
+            writer.WriteFixedString32((FixedString32Bytes)cardsChange.killedCards[i].gameObject.name);
+            writer.WriteByte((byte)cardsChange.killedCards[i].faction);
+            writer.WriteByte((byte)cardsChange.killedCards[i].cardType);
+        }
+
+
+        //Write the number of cards in the revivedCards list
+        writer.WriteByte((byte)cardsChange.revivedCards.Count);
+        //Write the cards in the revivedCards list
+        for (int i = 0; i < cardsChange.revivedCards.Count; i++)
+        {
+            writer.WriteByte((byte)cardsChange.revivedCards[i].manaCost);
+            writer.WriteByte((byte)cardsChange.revivedCards[i].attackValue);
+            writer.WriteByte((byte)cardsChange.revivedCards[i].defenseValue);
+            writer.WriteFixedString32((FixedString32Bytes)cardsChange.newCards[i].cardName);
+            writer.WriteByte((byte)cardsChange.revivedCards[i].faction);
+            writer.WriteByte((byte)cardsChange.revivedCards[i].cardType);
+        }
     }
 
 
