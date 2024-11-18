44add83d3a8c9f1f07213e503b012b54a79d9832
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2024-11-18 11:43:57 +0000

    Attempt at making the playing field synch class

--- Assets/Scripts/Networking/PlayingFieldSynch.cs
+++ Assets/Scripts/Networking/PlayingFieldSynch.cs
@@ -1,6 +1,3 @@
-using NUnit.Framework.Internal;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 ///TODO: Relating card Changes from client/host into moves: https://www.notion.so/finleyfooonfire/Decomposition-13c4b7e33ee880389e8be96f21928b4c
@@ -11,9 +8,33 @@ public class PlayingFieldSynch : MonoBehaviour
     [SerializeField] TMP_Text textIn;
     //END*/
 
+    [SerializeField] Transform cardPlayArea;
+    //Opponent
+    [SerializeField] Transform opponentGrave;
+    [SerializeField] Transform opponentHand;
+    [SerializeField] Transform opponentDeck;
+
+    //Returns a CardsChange with all the things that have changed since the start of the turn.
+    CardsChange GetCardStatus()
+    {
+        throw new System.NotImplementedException();
+    }
+
     //Uses the client/server to send data to the other device.
     void Send()
     {
+        CardsChange changes = GetCardStatus();
+        var client = FindAnyObjectByType<GameClient>();
+        if (client != null)
+        {
+            //The client exists so Send data.
+            client.SendToServer(changes);
+        }
+        else
+        {
+            //It is server. Send data.
+            FindAnyObjectByType<GameServer>().SendToClient(changes);
+        }
         throw new System.NotImplementedException();
     }
 
@@ -25,8 +46,66 @@ public class PlayingFieldSynch : MonoBehaviour
         textIn.text = "IN: " + recievedCardsUpdate.ToString();
         //END
         */
+        SetCardStatus(recievedCardsUpdate);
 
         throw new System.NotImplementedException();
+    }
+
+    //Updates the board using the changes recieved.
+    void SetCardStatus(CardsChange changeIn)
+    {
+        //Add new cards to the deck
+        foreach (var newCard in changeIn.newCards)
+        {
+            opponentDeck.GetComponent<Deck>().deckCards.Add(newCard);
+        }
+
+        //Move cards from the deck to the hand
+        foreach (var newCard in changeIn.drawnCards)
+        {
+            opponentDeck.GetComponent<Deck>().deckCards.Remove(newCard);
+            opponentDeck.GetComponent<Deck>().handCards.Add(newCard);
+        }
+
+        //Move cards from the hand to the play area
+        foreach (var card in changeIn.playedCards)
+        {
+            opponentDeck.GetComponent<Deck>().handCards.Remove(card);
+            GameObject cardObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //Keenan addition
+            cardObject.name = card.cardName;
+            //END
+            cardObject.transform.SetParent(cardPlayArea);
+            cardObject.transform.position = new Vector3();
+            cardObject.transform.localScale = new Vector3(0.635f, 0.01f, 0.889f);
+            cardObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            CardInfo cardInfo = cardObject.AddComponent<CardInfo>();
+            cardInfo.manaCost = card.manaCost;
+            cardInfo.attackValue = card.attackValue;
+            cardInfo.defenseValue = card.defenseValue;
+            cardInfo.faction = card.faction;
+            cardInfo.cardType = card.cardType;
+
+            //Keenan Addition
+            CardAttack cardAttack = cardObject.AddComponent<CardAttack>();
+            //End
+        }
+
+        //Update any cards that have changed. Only the defense value and exhausted variables change.
+        foreach (var card in changeIn.changedCards)
+        {
+            CardInfo oldCard = cardPlayArea.Find(card.Key).GetComponent<CardInfo>();
+            oldCard.defenseValue = card.Value.defenseValue;
+            oldCard.exhausted = card.Value.exhausted;
+        }
+
+        //Update any cards that have been killed.
+        foreach (var card in changeIn.killedCards) 
+        {
+            cardPlayArea.Find(card.Key).SetParent(opponentGrave);
+            cardPlayArea.Find(card.Key).transform.localPosition = new Vector3();
+        }
     }
 
     /*
