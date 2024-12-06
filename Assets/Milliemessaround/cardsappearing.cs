5f6ac209cadd238a5d72de6bf1c586674762bb21
milliedann <milliedann10@gmail.com>
2024-12-06 17:48:05 +0000

    programming drawing the cards

---
+++ Assets/Milliemessaround/cardsappearing.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class cardsappearing : MonoBehaviour
+{
+    public GameObject snapcard;
+    public GameObject[] player1card;
+    public GameObject[] player2card;
+
+    public void OnDealsnap ()
+    {
+        print("hola");
+        //snapcard.SetActive(true);
+        int i = 0;
+        while (i < player1card.Length)
+        {
+            player1card[i].SetActive(false);
+            i++;
+        }
+        int cardnumber = Random.Range(0, 5);
+        player1card[cardnumber].SetActive(true);
+
+        while (i < player2card.Length)
+        {
+            player2card[i].SetActive(false);
+            i++;
+        }
+        player2card[cardnumber].SetActive(true);
+    }
+
+
+
+}
