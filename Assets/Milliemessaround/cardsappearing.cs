bb2f7ac9f0bc8d9d93898d23fe0332dd41b685fb
milliedann <milliedann10@gmail.com>
2024-12-13 14:19:07 +0000

    tags are now working when it comes to snap

--- Assets/Milliemessaround/cardsappearing.cs
+++ Assets/Milliemessaround/cardsappearing.cs
@@ -16,7 +16,7 @@ public class cardsappearing : MonoBehaviour
 
     public void OnDealsnap ()
     {
-        
+        widget.SetActive(false);
 
         print("hola");
         //snapcard.SetActive(true);
@@ -25,28 +25,32 @@ public class cardsappearing : MonoBehaviour
         {
             player1card[i].SetActive(false);
             i++;
-            tag1 = player1card[i].tag;
-            print("1:" + i);
+            //tag1 = player1card[i];
+
         }
         int cardnumber = Random.Range(0, 5);
         player1card[cardnumber].SetActive(true);
-        tag1 = player1card[i];
+        tag1 = player1card[cardnumber];
+        print(cardnumber);
 
         i = 0;
         while (i < player2card.Length - 1)
         {
             player2card[i].SetActive(false);
             i++;
-            tag2 = player2card[i].tag;
-            print("2:" + i);
+            //tag2 = player2card[i];
+
         }
         cardnumber = Random.Range(0, 5);
         player2card[cardnumber].SetActive(true);
-        tag2 = player2card[i];
+        tag2 = player2card[cardnumber];
+        print(cardnumber);
 
         if (tag1.CompareTag(tag2.tag))
         {
             widget.SetActive(true);
+            print(tag1.tag);
+            print(tag2.tag);
         }
 
     }
